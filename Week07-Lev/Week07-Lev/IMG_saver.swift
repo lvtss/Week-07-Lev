7344c0fa412290045274f0597ae7a539fdefeb66
lvtss <112721368+lvtss@users.noreply.github.com>
2023-03-20 11:27:24 -0400

    innit

---
+++ Week07-Lev/Week07-Lev/IMG_saver.swift
@@ -0,0 +1,26 @@
+//
+//  IMG_Saver.swift
+//  Week07-Lev
+//
+//  Created by Levitia on 20/03/2023.
+//
+
+import Foundation
+import UIKit
+
+class ImageSaver: NSObject {
+    var successHandler: (() -> Void)?
+    var errorHandler: ((Error) -> Void)?
+
+    func writeToPhotoAlbum(image: UIImage) {
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(saveCompleted), nil)
+    }
+
+    @objc func saveCompleted(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            errorHandler?(error)
+        } else {
+            successHandler?()
+        }
+    }
+}
