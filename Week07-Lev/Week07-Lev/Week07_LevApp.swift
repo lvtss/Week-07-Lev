736b6aa315a9e9613bda669676ddacceb17fc9f3
lvtss <112721368+lvtss@users.noreply.github.com>
2023-03-20 10:46:30 -0400

    innit

---
+++ Week07-Lev/Week07-Lev/Week07_LevApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week07_LevApp.swift
+//  Week07-Lev
+//
+//  Created by Levitia on 20/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct Week07_LevApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
